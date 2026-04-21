52a87c63f63aaaa1f00e5be4f4e316cc57198f6e
Godenot-print <edu.yuri06@gmail.com>
2026-04-21 20:02:33 -0300

    alteraçoes nos botões reiniciar e sair

---
+++ src/js/reiniciar.js
@@ -0,0 +1,10 @@
+const botao = document.getElementById("btn-reiniciar")
+
+// esconder
+botao.style.display = "none";
+
+document.getElementById("btn-coletar").addEventListener("click", () => {
+botao.style.display = "block";
+
+}
+)
---
+++ src/js/sair.js
@@ -0,0 +1,14 @@
+document.getElementById("btn-sair").addEventListener("click", (e) => {
+    e.preventDefault(); // faz com que primeiro rode o código e depois vá para próxima página
+
+    // limpa os dados salvos
+    localStorage.removeItem("totalAposta");
+    localStorage.removeItem("saldoGlobal");
+    localStorage.removeItem("ganhoTotal");
+
+
+    window.location.href = "index.html";
+});
+
+
+
