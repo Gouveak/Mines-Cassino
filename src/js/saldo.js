3cc3d0f4239ca975de7a6737fdd1a2b896a18eb0
Lucas Fonseca <fonsecafluc@gmail.com>
2026-04-21 20:14:42 -0300

    3 - validação de saldo múltiplo de 10 e correção do input

--- src/js/saldo.js
+++ src/js/saldo.js
@@ -1,14 +1,29 @@
 const saldoEL = document.getElementById("saldoInput"); // const referente ao input
 
-function validarSaldo(saldoInput) { // função que retorna uma mensagem caso o valor inserido seja não finito, não inteiro, ou menor que 10
-  if (!Number.isFinite(saldoInput) || !Number.isInteger(saldoInput) || saldoInput < 10) return "Informe o valor de fichas corretamente.";
+function validarSaldo(saldoInput, valorOriginal) { // função que retorna uma mensagem caso o valor inserido seja não finito, não inteiro, menor que 10 ou não múltiplo de 10 ou o campo do input esteja vazio
+  if (valorOriginal === "") {
+    return "Por favor, insira o valor de fichas para continuar.";
+  }
+
+  if (!Number.isFinite(saldoInput) || !Number.isInteger(saldoInput)) {
+    return "Por favor, insira um valor inteiro.";
+  }
+
+  if (saldoInput < 10) {
+    return "O saldo mínimo permitido é 10 fichas.";
+  }
+
+  if (saldoInput % 10 !== 0) {
+    return "O saldo deve ser múltiplo de 10 (Ex: 10, 20, 50, 100).";
+  }
   return null;
 }
 
 document.getElementById("btnSalvarSaldo").addEventListener("click", () => { // evento referente ao botão, onde definimos que, caso a função de validação seja satisfeita, a mensagem seja exibida em um alert
+  const valorTexto = saldoEL.value;
   const saldo = Number(saldoEL.value);
 
-  const erro = validarSaldo(saldo);
+  const erro = validarSaldo(saldo, valorTexto);
   if (erro) {
     alert(erro);
     return;
